e5524f4a592373185bf08c74e2bcadd5f68b3ca8
Quý <96576295+quy-ndc@users.noreply.github.com>
2025-11-03 21:03:14 +0700

    (chore): Remove all semi colon from _layout

--- apps/mobile-test-app/app/(tabs)/_layout.tsx
+++ apps/mobile-test-app/app/(tabs)/_layout.tsx
@@ -1,13 +1,13 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from 'expo-router'
+import React from 'react'
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { HapticTab } from '@/components/haptic-tab'
+import { IconSymbol } from '@/components/ui/icon-symbol'
+import { Colors } from '@/constants/theme'
+import { useColorScheme } from '@/hooks/use-color-scheme'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
 
   return (
     <Tabs
@@ -29,7 +29,7 @@ export default function TabLayout() {
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
-      /> 
+      />
     </Tabs>
-  );
+  )
 }
