6742da513fd463eff3b538a9372f30d27ed973ef
Quý <96576295+quy-ndc@users.noreply.github.com>
2025-11-06 21:26:26 +0700

    (fix): Add style field to button props and styling change

--- packages/ui-mobile/src/ui/button/enum.ts
+++ packages/ui-mobile/src/ui/button/enum.ts
@@ -4,20 +4,20 @@ export const ButtonVariant = {
     OUTLINE: "outline",
     GHOST: "ghost",
     LINK: "link",
-} as const;
+} as const
 
 export const ButtonSize = {
     SMALL: "small",
     DEFAULT: "default",
     LARGE: "large",
-} as const;
+} as const
 
 export const ButtonRound = {
     SMALL: "small",
     DEFAULT: "default",
     FULL: "full",
-} as const;
+} as const
 
-export type ButtonVariant = typeof ButtonVariant[keyof typeof ButtonVariant];
-export type ButtonSize = typeof ButtonSize[keyof typeof ButtonSize];
-export type ButtonRound = typeof ButtonRound[keyof typeof ButtonRound];
+export type ButtonVariant = typeof ButtonVariant[keyof typeof ButtonVariant]
+export type ButtonSize = typeof ButtonSize[keyof typeof ButtonSize]
+export type ButtonRound = typeof ButtonRound[keyof typeof ButtonRound]
