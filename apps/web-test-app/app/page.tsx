077812f5b41e5570bf56db79c3700f25539af778
Lê Huy <huy110903@gmail.com>
2025-11-09 22:13:53 +0700

    feat: add button component

--- apps/web-test-app/app/page.tsx
+++ apps/web-test-app/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image"
+import { Button, ButtonVariant, ButtonRound } from "@reactwave/ui-web";
 
 export default function Home() {
+
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div style={{ display: "flex", gap: 12 }}>
+          <Button>Default</Button>
+          <Button variant={ButtonVariant.OUTLINE}>Outline</Button>
+          <Button variant={ButtonVariant.PRIMARY} rounded={ButtonRound.FULL}>Primary</Button>
+          <Button variant={ButtonVariant.DANGER} disabled>Delete</Button>
+          <Button variant={ButtonVariant.SECONDARY} loading>Loading...</Button>
+        </div>
         <Image
           className="dark:invert"
           src="/next.svg"
---
+++ packages/ui-web/src/ui/button/enum.tsx
@@ -0,0 +1,23 @@
+export enum ButtonVariant {
+    PRIMARY = "PRIMARY",
+    SECONDARY = "SECONDARY",
+    OUTLINE = "OUTLINE",
+    GHOST = "GHOST",
+    DANGER = "DANGER",
+}
+
+export enum ButtonSize {
+    XS = "XS",
+    SM = "SM",
+    DEFAULT = "DEFAULT",
+    LG = "LG",
+    XL = "XL",
+}
+
+export enum ButtonRound {
+    NONE = "NONE",
+    SM = "SM",
+    DEFAULT = "DEFAULT",
+    LG = "LG",
+    FULL = "FULL",
+}
